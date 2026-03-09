c13069496584868939a20737376cc35404a6fd90
nhanducho <nhanducvaa@gmail.com>
2026-03-09 08:02:41 +0100

    add

--- settings.py
+++ settings.py
@@ -68,7 +68,7 @@ DEBUG = False
 
 ADMIN_USERNAME = 'admin'
 # for security, best to set admin password in an environment variable
-ADMIN_PASSWORD = environ.get('nhanDucHo@1456')
+ADMIN_PASSWORD = environ.get('123456')
 
 DEMO_PAGE_INTRO_HTML = """ """
 
